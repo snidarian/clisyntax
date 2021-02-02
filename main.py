2d02732d5ef9b1ee21cf9e9b0f99cc4d601013a1
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-02 09:11:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 
 
 class ProgrammingLanguage:
-    def __init__(self, name, paradigm, typing, creators, first_appeared, history, wiki_link):
+    def __init__(self, name, paradigm, typing, creators, first_appeared, history, wiki_link, version):
         self.name = name  # both
         self.paradigm = paradigm  # technical
         self.typing = typing  # technical
@@ -13,10 +13,12 @@ class ProgrammingLanguage:
         self.first_appeared = first_appeared  # historical
         self.history = history  # historical
         self.wiki_link = wiki_link  # both
+        self.version = version
 
     def output_language_classifications(self):
         print(self.name + " is a " + self.paradigm + " language with " + self.typing + " typing discipline"
-              "for more information, visit the language's wikipedia page: " + self.wiki_link)
+              "for more information, visit the language's wikipedia page: " + self.wiki_link +
+              "\nThe version represented here is: " + self.version)
 
     def output_historical_information(self):
         print(self.name + " was created by " + self.creators + " first appearing in " + self.first_appeared + ". "
@@ -24,10 +26,12 @@ class ProgrammingLanguage:
 
 
 class LanguageSyntax:
-    def __init__(self, options_list, data_types, operators, for_loop, while_loop, case_statement, class_definition,
-                 define_variable, array, function_definition, control_flow, user_input, output, arithmetic,
-                 arithmetic_operators, comparison_operators, logical_operators, builtin_functions, hello_world):
+    def __init__(self, options_list, keywords, data_types, operators, for_loop, while_loop, case_statement,
+                 class_definition, define_variable, array, function_definition, control_flow, user_input, output,
+                 arithmetic, arithmetic_operators, comparison_operators, logical_operators, builtin_functions,
+                 hello_world):
         self.options = options_list
+        self.keywords = keywords
         self.data_types = data_types
         self.operators = operators
         self.for_loop = for_loop
@@ -60,38 +64,42 @@ class LanguageSyntax:
 # ProgrammingLanguage object instantiations
 
 LISP_lang = ProgrammingLanguage("Common LISP", "Multi-paradigm: functional, procedural, reflective, meta",
-                    "Dynamic, strong", "John McCarthy", "1958",
-                    "John McCarthy developed Lisp in 1958 while at the Massachusetts Institute of Technology (MIT)",
-                    "https://en.wikipedia.org/wiki/Lisp_(programming_language)")
+                                "Dynamic, strong", "John McCarthy", "1958",
+                                "John McCarthy developed Lisp in 1958 while at"
+                                " the Massachusetts Institute of Technology (MIT)",
+                                "https://en.wikipedia.org/wiki/Lisp_(programming_language)",
+                                "Common LISP")
 
-bash_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+bash_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-python3_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+python3_lang = ProgrammingLanguage("", "", "", "", "", "", "", "python3.9")
 
-javascript_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+javascript_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-c_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+c_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-cpp_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+cpp_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-java_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+java_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-perl_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+perl_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-ruby_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+ruby_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-next0_lang = ProgrammingLanguage("", "", "", "", "", "", "")
+next0_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
-next1_lang = ProgrammingLanguage("", "", "", "", "", "", "")
-
+next1_lang = ProgrammingLanguage("", "", "", "", "", "", "", "")
 
 
 # Variable definitions
 dash_band = "-------------------------------------------------------\n"
 
+
 # LanguageSyntax object class instantiations
 
-LISP_syntax = LanguageSyntax("options", "data types",
+LISP_syntax = LanguageSyntax("options",
+                             "keywords",
+                             "data types",
                              dash_band +
                              "Arithmetic Operators:\n+\t-\t*\t/\nmod|rem\tincf\tdecf\t\n"
                              "Comparison Operators:\n=\t/=\t>\t<\n>=\t<=\tmax\tmin\n"
@@ -118,27 +126,31 @@ LISP_syntax = LanguageSyntax("options", "data types",
                              "builtin functions",
                              "hello world")
 
+
 bash_syntax = LanguageSyntax("options",
-                                "data types",
-                                "operators",
-                                "forloop",
-                                "whileloop",
-                                "case statements",
-                                "class definition",
-                                "variable definition",
-                                "arrays",
-                                "function definition",
-                                "control flow",
-                                "user input",
-                                "output",
-                                "arithmetic",
-                                "arithmetic operators",
-                                "comparison operators",
-                                "logical operators",
-                                "builtin functions",
-                                "hello world")
+                             "keywords",
+                             "data types",
+                             "operators",
+                             "forloop",
+                             "whileloop",
+                             "case statements",
+                             "class definition",
+                             "variable definition",
+                             "arrays",
+                             "function definition",
+                             "control flow",
+                             "user input",
+                             "output",
+                             "arithmetic",
+                             "arithmetic operators",
+                             "comparison operators",
+                             "logical operators",
+                             "builtin functions",
+                             "hello world")
+
 
 python3_syntax = LanguageSyntax("options",
+                                "Keywords\t\tdescription",
                                 "data types",
                                 "operators",
                                 "forloop",
@@ -158,47 +170,69 @@ python3_syntax = LanguageSyntax("options",
                                 "builtin functions",
                                 "hello world")
 
-c_syntax = LanguageSyntax(      "options",
-                                "data types",
-                                "operators",
-                                "forloop",
-                                "whileloop",
-                                "case statements",
-                                "class definition",
-                                "variable definition",
-                                "arrays",
-                                "function definition",
-                                "control flow",
-                                "user input",
-                                "output",
-                                "arithmetic",
-                                "arithmetic operators",
-                                "comparison operators",
-                                "logical operators",
-                                "builtin functions",
-                                "hello world")
+c_syntax = LanguageSyntax("options",
+                          "keywords",
+                          "data types",
+                          "operators",
+                          "forloop",
+                          "whileloop",
+                          "case statements",
+                          "class definition",
+                          "variable definition",
+                          "arrays",
+                          "function definition",
+                          "control flow",
+                          "user input",
+                          "output",
+                          "arithmetic",
+                          "arithmetic operators",
+                          "comparison operators",
+                          "logical operators",
+                          "builtin functions",
+                          "hello world")
 
 cpp_syntax = LanguageSyntax("options",
-                                "data types",
-                                "operators",
-                                "forloop",
-                                "whileloop",
-                                "case statements",
-                                "class definition",
-                                "variable definition",
-                                "arrays",
-                                "function definition",
-                                "control flow",
-                                "user input",
-                                "output",
-                                "arithmetic",
-                                "arithmetic operators",
-                                "comparison operators",
-                                "logical operators",
-                                "builtin functions",
-                                "hello world")
+                            "keywords",
+                            "data types",
+                            "operators",
+                            "forloop",
+                            "whileloop",
+                            "case statements",
+                            "class definition",
+                            "variable definition",
+                            "arrays",
+                            "function definition",
+                            "control flow",
+                            "user input",
+                            "output",
+                            "arithmetic",
+                            "arithmetic operators",
+                            "comparison operators",
+                            "logical operators",
+                            "builtin functions",
+                            "hello world")
 
 
+insert_lang_here = LanguageSyntax("options",
+                            "keywords",
+                            "data types",
+                            "operators",
+                            "forloop",
+                            "whileloop",
+                            "case statements",
+                            "class definition",
+                            "variable definition",
+                            "arrays",
+                            "function definition",
+                            "control flow",
+                            "user input",
+                            "output",
+                            "arithmetic",
+                            "arithmetic operators",
+                            "comparison operators",
+                            "logical operators",
+                            "builtin functions",
+                            "hello world")
 
 
 parser = argparse.ArgumentParser()
@@ -222,6 +256,10 @@ if "lisp" in args.language:
         LISP_lang.output_historical_information()
     elif args.syntax == "options":
         LanguageSyntax.display_syntax_selection_options()
+    elif args.syntax == "keywords":
+        print(LISP_syntax.user_input)
+        if args.verbose:
+            print("")
     elif args.syntax == "datatypes":
         print(LISP_syntax.user_input)
         if args.verbose:
@@ -283,15 +321,97 @@ if "lisp" in args.language:
     else:
         print("Error: syntactic query not found: try --help")
 
+# PYTHON3
+
 elif "python" in args.language:
     if args.syntax == "classification":
-        print("Paradigm: Multi-paradigm:\n\tfunctional, imperative, object-oriented, structured, reflective")
-        print("Designed by:\n\tGuido van Rossum")
-        print("Typing Discipline:\n\tDuck, dynamic, strong typing")
+        python3_lang.output_language_classifications()
+        if args.verbose:
+            print("")
+    elif args.syntax == "history" or args.syntax == "historical":
+        python3_lang.output_historical_information()
+        if args.verbose:
+            print("")
+    elif args.syntax == "options":
+        LanguageSyntax.display_syntax_selection_options()
+        if args.verbose:
+            print("")
+    elif args.syntax == "keywords":
+        print(python3_syntax.keywords)
+        if args.verbose:
+            print("")
+    elif args.syntax == "datatypes":
+        print(python3_syntax.data_types)
+        if args.verbose:
+            print("")
+    elif args.syntax == "operators":
+        print(python3_syntax.operators)
+        if args.verbose:
+            print("")
     elif args.syntax == "forloop":
-        print("for loopvar in list")
-    elif args.syntax == "whileloop" or "while" in args.syntax:
-        print("mark 4")
+        print(python3_syntax.forloop)
+        if args.verbose:
+            print("")
+    elif args.syntax == "whileloop":
+        print(python3_syntax.while_loop)
+        if args.verbose:
+            print("")
+    elif args.syntax == "casestatement" or "case" in args.syntax:
+        print(python3_syntax.case_statement)
+        if args.verbose:
+            print("")
+    elif args.syntax == "defineclass" or "class" in args.syntax:
+        print(python3_syntax.define_class)
+        if args.verbose:
+            print("")
+    elif args.syntax == "definevariable" or "variable" in args.syntax:
+        print(python3_syntax.variable_definition)
+        if args.verbose:
+            print("")
+    elif args.syntax == "array":
+        print(python3_syntax.array)
+        if args.verbose:
+            print("")
+    elif args.syntax == "function":
+        print(python3_syntax.function_definition)
+        if args.verbose:
+            print("")
+    elif args.syntax == "controlflow":
+        print(python3_syntax.control_flow)
+        if args.verbose:
+            print("")
+    elif args.syntax == "input":
+        print(python3_syntax.user_input)
+        if args.verbose:
+            print("")
+    elif args.syntax == "output":
+        print(python3_syntax.output)
+        if args.verbose:
+            print("")
+    elif args.syntax == "arithmetic":
+        print(python3_syntax.arithmetic)
+        if args.verbose:
+            print("")
+    elif args.syntax == "arithmetic operators" or ("arithmetic" and "operators") in args.syntax: # works
+        print(python3_syntax.arithmetic_operators)
+        if args.verbose:
+            print("mark1")
+    elif args.syntax == "comparisonoperators":
+        print(python3_syntax.comparison_operators)
+        if args.verbose:
+            print("")
+    elif args.syntax == "logicaloperators":
+        print(python3_syntax.logical_operators)
+        if args.verbose:
+            print("")
+    elif args.syntax == "builtinfunctions":
+        print(python3_syntax.builtin_functions)
+        if args.verbose:
+            print("")
+    elif args.syntax == "helloworld":
+        print(python3_syntax.hello_world)
+        if args.verbose:
+            print("")
     else:
         print("Error: syntactic query not found: try --help")
 
@@ -299,7 +419,6 @@ elif "javascript" in args.language:
     print("mark 5")
 else:
     print("Error: First argument - [Language] - not supported or else not recognized")
-
 
 
 
